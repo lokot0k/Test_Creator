bf1c45bd78a7cce810d32ec440c951de1c07f082
Alexey <kaptoxa322@mail.ru>
2020-11-18 21:42:48 +0300

    dialog with test data

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QFileDialog
 from main_ui import Ui_MainWindow
+from dialog_test import Ui_Dialog
+import sys
+
+
+class TestInfoStruct:
+    def __init__(self):
+        self.name = None
+        self.count = None
+        self.path = None
+
+
+inform = TestInfoStruct()
+
+
+class DialogTest(QWidget, Ui_Dialog):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.reciever)
+
+    def accept(self):
+        inform.name = self.name_input.text()
+        inform.count = int(self.spinBox.text())
+
+    def reject(self):
+        self.close()
+
+    def reciever(self):
+        self.path = QFileDialog.getExistingDirectory(self, 'Выберите папку: ', '')
+        self.choose.setText('Директория для сохранения файлов: ' + self.path)
 
 
 class MainWidget(QMainWindow, Ui_MainWindow):
@@ -8,9 +38,19 @@ class MainWidget(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.CreateTest.clicked.connect(self.create_test)
         self.PassTest.clicked.connect(self.pass_test)
+        self.setFixedSize(800, 500)
 
     def create_test(self):
-        print('create')
+        self.test_dialog = DialogTest()
+
+        self.test_dialog.show()
 
     def pass_test(self):
         print('pass')
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MainWidget()
+    ex.show()
+    sys.exit(app.exec_())
